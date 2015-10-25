9071edd05dcae4ad5474c44f50283ff3d73b8b32
Josh Woodcock <josh.woodcock@opentimeapp.com>
2015-10-24 19:21:05 -0500

    Initial Commit

---
+++ OpenTimeSDK/OpenTimeSDK.h
@@ -0,0 +1,19 @@
+//
+//  OpenTimeSDK.h
+//  OpenTimeSDK
+//
+//  Created by Josh Woodcock on 10/24/15.
+//  Copyright © 2015 Connecting Open Time, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenTimeSDK.
+FOUNDATION_EXPORT double OpenTimeSDKVersionNumber;
+
+//! Project version string for OpenTimeSDK.
+FOUNDATION_EXPORT const unsigned char OpenTimeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenTimeSDK/PublicHeader.h>
+
+
